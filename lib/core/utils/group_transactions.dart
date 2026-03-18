da52667590c2f5be49beb778f3a739f5f8944c15
p1xleon <wizmonkey1337@gmail.com>
2026-03-18 22:52:26 +0530

    update: udpate transactions ui

--- lib/core/utils/group_transactions.dart
+++ lib/core/utils/group_transactions.dart
@@ -2,30 +2,62 @@ import 'package:intl/intl.dart';
 
 import '../../features/transactions/data/models/transaction_model.dart';
 
-Map<String, List<TransactionModel>> groupTransactionsByMonth(
+final DateFormat _monthKeyFormatter = DateFormat('MMMM yyyy');
+
+List<TransactionMonthGroup> groupTransactionsByMonth(
   List<TransactionModel> transactions,
 ) {
-  final Map<String, List<TransactionModel>> grouped = {};
+  final Map<int, List<TransactionModel>> grouped = {};
 
   for (final tx in transactions) {
-    final monthKey = DateFormat('MMMM yyyy').format(tx.date);
+    final monthKey = tx.date.year * 100 + tx.date.month;
 
     grouped.putIfAbsent(monthKey, () => []);
     grouped[monthKey]!.add(tx);
   }
 
-  // Sort transactions inside each month
   for (final list in grouped.values) {
     list.sort((a, b) => b.date.compareTo(a.date));
   }
 
-  // Sort months (latest first)
-  final sortedEntries = grouped.entries.toList()
-    ..sort((a, b) {
-      final dateA = DateFormat('MMMM yyyy').parse(a.key);
-      final dateB = DateFormat('MMMM yyyy').parse(b.key);
-      return dateB.compareTo(dateA);
-    });
+  final sortedKeys = grouped.keys.toList()..sort((a, b) => b.compareTo(a));
 
-  return Map.fromEntries(sortedEntries);
+  return sortedKeys
+      .map((key) {
+        final year = key ~/ 100;
+        final month = key % 100;
+        final monthTransactions = grouped[key]!;
+
+        double income = 0;
+        double expense = 0;
+        for (final tx in monthTransactions) {
+          if (tx.type == TransactionType.income) {
+            income += tx.amount;
+          } else {
+            expense += tx.amount;
+          }
+        }
+
+        return TransactionMonthGroup(
+          title: _monthKeyFormatter.format(DateTime(year, month)),
+          transactions: monthTransactions,
+          income: income,
+          expense: expense,
+        );
+      })
+      .toList(growable: false);
+}
+
+class TransactionMonthGroup {
+  final String title;
+  final List<TransactionModel> transactions;
+  final double income;
+  final double expense;
+
+  const TransactionMonthGroup({
+    required this.title,
+    required this.transactions,
+    required this.income,
+    required this.expense,
+  });
 }
