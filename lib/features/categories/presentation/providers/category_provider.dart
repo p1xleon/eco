8953b5fa88777afcf6d67bb25cdc4b692035b632
p1xleon <wizmonkey1337@gmail.com>
2026-03-31 17:22:05 +0530

    update: update category provider

--- lib/features/categories/presentation/providers/category_provider.dart
+++ lib/features/categories/presentation/providers/category_provider.dart
@@ -1,11 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../core/auth/auth_provider.dart';
 import '../../data/models/category_model.dart';
 import '../../data/repositories/category_repository.dart';
+import '../../data/providers/category_remote_provider.dart';
 
-final categoryRepositoryProvider = Provider((ref) => CategoryRepository());
+final categoryRepositoryProvider = Provider<CategoryRepository>((ref) {
+  final remote = ref.read(categoryRemoteSourceProvider);
+  return CategoryRepository(remote);
+});
 
 final categoriesProvider = FutureProvider<List<CategoryModel>>((ref) async {
+  ref.watch(authStateProvider);
   final repo = ref.read(categoryRepositoryProvider);
   return repo.getAll();
 });
